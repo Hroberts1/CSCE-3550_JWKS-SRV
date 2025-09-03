e9be9af8037573289862ee3dfd6a8e64766feafd
Hroberts1 <hunter@hunterstechlab.com>
2025-09-03 07:43:55 -0500

    refactor: enhance configuration structure and initialization in httpserver module

--- internal/httpserver/config.go
+++ internal/httpserver/config.go
@@ -2,13 +2,48 @@ package httpserver
 
 import (
 	"fmt"
-	"os"
 	"time"
 )
 
 const (
-	defaultIssuer = "jwks-server"
+	defaultIssuer      = "jwks-server"
 	defaultJWTLifetime = "5m"
-	defaultKeyRetain = "1h"
+	defaultKeyRetain   = "1h"
 	defaultKeyLifetime = "10m"
 )
+
+type Config struct {
+	KeyLifetime     time.Duration
+	KeyRetainPeriod time.Duration
+	JWTLifetime     time.Duration
+	Issuer          string
+}
+
+func NewConfig() (*Config, error) {
+	// setup the defaults
+	issuer := defaultIssuer
+
+	keyLifetime, err := time.ParseDuration(defaultKeyLifetime)
+	if err != nil {
+		return nil, fmt.Errorf("invalid defaultKeyLifetime: %w", err)
+	}
+
+	keyRetain, err := time.ParseDuration(defaultKeyRetain)
+	if err != nil {
+		return nil, fmt.Errorf("invalid defaultKeyRetain: %w", err)
+	}
+
+	jwtLifetime, err := time.ParseDuration(defaultJWTLifetime)
+	if err != nil {
+		return nil, fmt.Errorf("invalid defaultJWTLifetime: %w", err)
+	}
+
+	// OverRide w/ env vars IF set AND valid
+
+	return &Config{
+		KeyLifetime:     keyLifetime,
+		KeyRetainPeriod: keyRetain,
+		JWTLifetime:     jwtLifetime,
+		Issuer:          issuer,
+	}, nil
+}
